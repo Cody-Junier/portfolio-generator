f926ca4f17a06fb55550042f8094fb365b6f11e2
Cody-Junier <90635812+Cody-Junier@users.noreply.github.com>
2021-11-30 15:53:12 -0600

    Finished 9.2.7

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length)
-console.log(profileDataArgs)
+const fs = require('fs')
+const generatePage= require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// console.log(profileDataArgs)
+const [name, github] = profileDataArgs;
+// const printProfileData = profileDataArr =>{
+//     for (let i=0; i < profileDataArr.length; i++){
+//     console.log(profileDataArr[i]);
+//     }
+//     console.log('==========');
+//     profileDataArr.forEach(profileItem =>
+//         console.log(profileItem));
+// };
 
-const printProfileData = profileDataArr =>{
-    for (let i=0; i < profileDataArr.length; i++){
-    console.log(profileDataArr[i]);
-    }
-    console.log('==========');
-    profileDataArr.forEach(profileItem =>
-        console.log(profileItem));
-};
+// printProfileData(profileDataArgs);
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
