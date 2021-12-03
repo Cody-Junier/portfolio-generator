5695bfa35662171b1401c232e12bf81a26644100
Cody-Junier <90635812+Cody-Junier@users.noreply.github.com>
2021-12-03 11:06:10 -0600

    9.3.5.5

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const inquirer = require('inquirer');
+inquirer
+    .prompt([{
+        type:'input',
+        name:'name',
+        message:'What is your name?'
+    }])
+    .then(answers => console.log(answers));
 // const fs = require('fs')
 // const generatePage= require('./src/page-template.js');
 
